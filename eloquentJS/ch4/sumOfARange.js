0358a2b97029874a740b03af63e0dd60ea318ef7
Jose Medina <josemedina921@gmail.com>
2016-08-28 22:12:37 -0700

    completing sum of a range

--- eloquentJS/ch4/sumOfARange.js
+++ eloquentJS/ch4/sumOfARange.js
@@ -1,15 +1,34 @@
-function range(start, end) {
-  var r = [];
+function range(start, end, step) {
   var i = start;
+  step = step || 1;
 
-  while( i <= end) {
-    r.push(i);
-    i += 1;
+  function stepper(start, end, comp) {
+     var r = [];
+     while(comp(i,end)) {
+        r.push(i);
+        i += step;
+     }
+     return r; 
+  }
+  
+  if(step < 0 && start < end) {
+    return [];
+  }
+  else if(step < 0) {
+    return stepper(start,end, (i, e) => i >= e);
   }
 
-  return r;
+  return stepper(start,end, (i, e) => i <= e);
 }
 
 function sum(nums) {
-  return Math.sum.apply(Math, nums);
+  return nums.reduce((total, next) => total + next, 0);
 }
+
+console.log(range(1,5));
+console.log(range(1,5,2));
+console.log(range(1,5, -2));
+console.log(range(5, 1, -2));
+console.log(range(5,1, -1));
+console.log(range(5,1));
+console.log(sum(range(1,5)));
