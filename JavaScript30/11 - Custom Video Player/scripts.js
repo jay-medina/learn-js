06929f1f362bd0a94c7543c8476ac769dadc4d4b
Jose Medina <josemedina921@gmail.com>
2017-01-08 13:32:09 -0800

    adding volume, playback and skip buttons

--- JavaScript30/11 - Custom Video Player/scripts.js
+++ JavaScript30/11 - Custom Video Player/scripts.js
@@ -2,6 +2,13 @@ const player = document.querySelector('.player');
 const video = player.querySelector('.player__video');
 
 const playButton = player.querySelector('.player__button');
+const volume = player.querySelector('input[name="volume"]');
+const playbackRate = player.querySelector('input[name="playbackRate"]');
+const skipBack = player.querySelector('button[data-skip="-10"]');
+const skipForward = player.querySelector('button[data-skip="25"]');
+
+video.volume = volume.value;
+video.playbackRate = playbackRate.value;
 
 playButton.addEventListener('click', function() {
   if(video.paused) {
@@ -11,3 +18,21 @@ playButton.addEventListener('click', function() {
     video.pause();
   }
 });
+
+volume.addEventListener('change', function(e) {
+  video.volume = volume.value;
+});
+
+playbackRate.addEventListener('change', function() {
+  video.playbackRate = playbackRate.value;
+});
+
+
+skipBack.addEventListener('click', function() {
+  video.currentTime -= 10;
+});
+
+skipForward.addEventListener('click', function() {
+  video.currentTime += 25;
+});
+
