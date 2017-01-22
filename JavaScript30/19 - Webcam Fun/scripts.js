90bf535be8f92ee9d6d96486ed801ba02e90a036
Jose Medina <josemedina921@gmail.com>
2017-01-21 16:29:01 -0800

    removing the event listener from html to script

--- JavaScript30/19 - Webcam Fun/scripts.js
+++ JavaScript30/19 - Webcam Fun/scripts.js
@@ -3,3 +3,37 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+  navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: false
+  }).then(localMediaStream => {
+    video.src = window.URL.createObjectURL(localMediaStream);
+    video.play();
+  }).catch(function(err) {
+    console.err(err);
+  });
+}
+
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+  return setInterval(function() {
+    ctx.drawImage(video, 0,0, width,height);
+  }, 24);
+  console.log(width,height)
+}
+
+function takePhoto() {
+  snap.currentTime = 0;
+  snap.play();
+}
+
+getVideo();
+
+const controls = document.querySelector('.controls');
+const takePhotoBtn = controls.addEventListener('click', takePhoto);
