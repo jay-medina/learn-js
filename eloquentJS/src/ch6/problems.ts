78c00e865e08287badb43a57cb53d7db019863a5
Jose Medina <josemedina921@gmail.com>
2018-12-30 13:57:36 -0800

    iterable groups

--- eloquentJS/src/ch6/problems.ts
+++ eloquentJS/src/ch6/problems.ts
@@ -21,3 +21,55 @@ export class Vec {
         return Math.sqrt(x2 + y2);
     }
 }
+
+function groupIterator<T>(items: T[]): Iterator<T> {
+    let currentIndex = 0;
+    const { length } = items;
+
+    return {
+        next(): IteratorResult<T> {
+            if (currentIndex === length) return { done: true, value: undefined! };
+
+            const val = items[currentIndex];
+            currentIndex = currentIndex + 1;
+
+            return { value: val, done: false };
+        },
+    };
+}
+
+export class Group<T> implements Iterable<T> {
+    private items: T[];
+
+    constructor() {
+        this.items = [];
+    }
+
+    add(item: T) {
+        if (!this.items.includes(item)) {
+            this.items.push(item);
+        }
+    }
+
+    remove(item: T) {
+        this.items = this.items.filter(i => i !== item);
+    }
+
+    has(item: T): boolean {
+        return this.items.includes(item);
+    }
+
+    get length() {
+        return this.items.length;
+    }
+
+    [Symbol.iterator](): Iterator<T> {
+        return groupIterator(this.items);
+    }
+
+    static from<X>(arr: X[]): Group<X> {
+        const g = new Group<X>();
+        arr.forEach(i => g.add(i));
+        return g;
+    }
+}
